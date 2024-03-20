e5bd9f2df162a014345ad2884279400280eb1358
jayson <jaysonshammah3@gmail.com>
2024-03-20 09:22:36 +0300

    add functions continued

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+
+function mondayWork(plan = "go to the office"){
+    return `This Monday, I will ${plan}.`
+}
+
+function wrapAdjective(flair = "*"){
+    return function(adjective) {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+        
+    }
+    
+
